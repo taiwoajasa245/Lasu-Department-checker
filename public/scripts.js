0ce87952aaf8f4f8a0c324ce0065c375ef79aeff
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-04-08 09:03:44 +0100

    project created

---
+++ public/scripts.js
@@ -0,0 +1,39 @@
+// Update the department select input based on the selected faculty and level 
+
+document.addEventListener("DOMContentLoaded", function () {
+    let level;
+    console.log("hola");
+
+    document
+      .getElementById("level")
+      .addEventListener("change", function () {
+        level = this.value;
+    });
+
+    document.getElementById("fac").addEventListener("change", function () {
+      var selectedFac = this.value;
+      // Encode the data in URL-encoded format
+      var formData = new URLSearchParams();
+      formData.append("fac", selectedFac, "level", level);
+
+      fetch("http://localhost:3000/get-dept", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: formData, // Use the encoded data
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.text();
+        })
+        .then((data) => {
+          document.getElementById("dept").innerHTML = data;
+        })
+        .catch((error) => {
+          console.error("Fetch error:", error);
+        });
+    });
+});
