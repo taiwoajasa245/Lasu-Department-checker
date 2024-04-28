cd8fc6372d5666de05f01d6d23794c4da252739d
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-04-28 03:28:32 +0100

    fixing issue with swagger

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const swaggerSpec = require('./specs/swaggerSpec.js');
 
 
 // Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }));
 
 
 // middleware
