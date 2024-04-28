282d65f559053837ff51fa1954bc37f181f1856a
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-04-28 13:27:46 +0100

    initial commit

--- server.js
+++ server.js
@@ -8,7 +8,47 @@ const courseRoute = require('./routes/courseRoute');
 
 // swagger documentation testing
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swaggerSpec.js');
+// const swaggerSpec = require('./swaggerSpec.js');
+
+
+
+
+
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'LASU Course API ',
+      version: '1.0.0',
+      description: 'get the list of all courses with the Course Title , Course Code , The UNIT of the course , LEVEL, Course Status',
+    },
+  },
+  apis:  ["routes/*.js"], // Path to your API route files
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Serve Swagger UI
 
